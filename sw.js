0b2ec646a25d6358f880ded855d7510475360cc7
Shivendra Saurav <shivendrasaurav@gmail.com>
2019-09-02 12:57:36 +0530

    Pushing manifest.json and sw.js

--- sw.js
+++ sw.js
@@ -6,6 +6,8 @@ const staticAssets = [
   'https://dscreva.github.io/index.html',
   'https://dscreva.github.io/team.html',
   'https://dscreva.github.io/events.html',
+  'https://dscreva.github.io/manifest.json',
+  'https://dscreva.github.io/sw.js',
 ];
 
 self.addEventListener('install', async e => {
