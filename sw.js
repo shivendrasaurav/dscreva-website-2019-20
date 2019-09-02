eeef6837b906ea5e4b683042cef39d1e49d6852a
Shivendra Saurav <shivendrasaurav@gmail.com>
2019-09-02 12:46:21 +0530

    Trial

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 const cacheName = 'dscreva';
 const staticAssets = [
   'https://dscreva.github.io/',
+  'https://dscreva.github.io/*',
   'https://dscreva.github.io/index.html',
   'https://dscreva.github.io/team.html',
   'https://dscreva.github.io/events.html',
